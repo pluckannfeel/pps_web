602a11f8d2a009fb6a3231120809fb384033fd9f
pluckannfeel <padayao.jm96@gmail.com>
2022-07-21 20:47:16 +0900

    added custom hook for http requests

---
+++ src/components/hooks/use-httprequest.tsx
@@ -0,0 +1,52 @@
+import { useState, useCallback } from 'react';
+import { Type } from 'typescript';
+
+type method = 'GET' | 'POST' | 'PUT' | 'DELETE';
+
+interface requestConfigProps {
+    url: string;
+    method: method;
+    headers: {};
+    body: {};
+}
+
+const useHttpRequest = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const sendRequest = useCallback(
+        async (requestConfig: requestConfigProps, applyData: (data: {}) => void) => {
+            setLoading(true);
+            setError(null);
+
+            try {
+                const response = await fetch(requestConfig.url, {
+                    method: requestConfig.method ? requestConfig.method : 'GET',
+                    headers: requestConfig.headers ? requestConfig.headers : {},
+                    body: requestConfig.body
+                        ? JSON.stringify(requestConfig.body)
+                        : null
+                });
+
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+
+                const data = await response.json();
+                applyData(data);
+            } catch (err: any) {
+                setError(err);
+            }
+            setLoading(false);
+        },
+        []
+    );
+
+    return {
+        loading,
+        error,
+        sendRequest
+    }
+};
+
+export default useHttpRequest;
